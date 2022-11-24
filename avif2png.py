781781db84a626c5326809ebe24d2e8d5e4927c0
komaame9 <fukano@DESKTOP-6HRETB9.localdomain>
2022-11-24 18:49:46 +0900

    refactored

--- avif2png.py
+++ avif2png.py
@@ -2,9 +2,6 @@ import pillow_avif
 from PIL import Image
 import sys
 
-print(sys.argv)
-
-filename = ""
 for arg in sys.argv:
     if arg.endswith(".avif"):
         filename_src = arg
@@ -12,5 +9,5 @@ for arg in sys.argv:
         
         image = Image.open(filename_src)
         image.save(filename_dst)
-        print('saved' + filename_dst)
+        print('saved:' + filename_dst)
 
